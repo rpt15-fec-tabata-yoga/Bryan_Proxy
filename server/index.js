4389920fa6cbf1cdc5b76ab6fd55e68adc46a8cc
Bryan Nguyen <bryan.m.nguyen12@gmail.com>
2019-08-27 19:47:02 -0700

    Added respective service servers. Work in progress.

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use(express.static('public'));
+app.use('/:gameId', express.static('gameId'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
